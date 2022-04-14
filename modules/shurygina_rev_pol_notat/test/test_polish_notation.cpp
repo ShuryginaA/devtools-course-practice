213913e9e7495a98e4e17c0f8e5090605880cd89
ShuryginaA <annashurygina353@gmail.com>
2022-04-14 11:58:01 +0300

    increase coverage

--- modules/shurygina_rev_pol_notat/test/test_polish_notation.cpp
+++ modules/shurygina_rev_pol_notat/test/test_polish_notation.cpp
@@ -3,6 +3,68 @@
 #include <gtest/gtest.h>
 #include <string>
 #include "include/polish_notation.h"
+#include "include/queue.h"
+
+
+TEST(TQueue, can_create_queue_with_positive_length) {
+    ASSERT_NO_THROW(TQueue<int> s(5));
+}
+
+
+TEST(TQueue, throws_when_create_queue_with_negative_length) {
+    ASSERT_ANY_THROW(TQueue<int> s(-5));
+}
+
+TEST(TQueue, can_create_copied_queue) {
+    TStruct<int>* s = new TQueue<int>;
+
+    ASSERT_NO_THROW(TStruct<int>*s1(s));
+}
+
+TEST(TQueue, can_push_and_pop) {
+    TStruct<int>* s = new TQueue<int>;
+    s->push(3);
+    int t = s->pop();
+    EXPECT_EQ(3, t);
+}
+
+
+TEST(TQueue, can_assign_queue_to_itself) {
+    TStruct<int>* s = new TQueue<int>;
+    TStruct<int>* s1(s);
+    s = s;
+    EXPECT_EQ(s, s1);
+}
+
+
+TEST(TQueue, check_void) {
+    TStruct<int>* s = new TQueue<int>;
+    EXPECT_EQ(true, s->isEmpty());
+}
+TEST(TQueue, check_fullness) {
+    TStruct<int>* s = new TQueue<int>(2);
+    s->push(3);
+    EXPECT_EQ(true, s->isFull());
+}
+
+TEST(Lexem, can_create) {
+    ASSERT_NO_THROW(Lexem l("2", value, 2));
+}
+
+TEST(Lexem, can_get_type) {
+    Lexem l("2", value, 2);
+    EXPECT_EQ(l.getType(), value);
+}
+
+TEST(Lexem, can_get_val) {
+    Lexem l("2", value, 2);
+    EXPECT_EQ(l.getVal(), 2);
+}
+
+TEST(Lexem, can_get_s) {
+    Lexem l("2", value, 2);
+    EXPECT_EQ(l.getS(), "2");
+}
 
 TEST(PolishNotation, Check_If_Numb_True) {
     PolishNotation l;
