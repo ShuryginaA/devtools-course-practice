90bab8dd40e857af4e249c213d02496f43e91948
ShuryginaA <annashurygina353@gmail.com>
2022-04-20 14:25:31 +0300

    cov

--- modules/shurygina_rev_pol_notat/include/queue.h
+++ modules/shurygina_rev_pol_notat/include/queue.h
@@ -23,12 +23,57 @@ class TQueue : public TStruct<T> {
 };
 
 template<class T>
+TQueue<T>::TQueue(const TQueue& v) {
+    N = v.N;
+    first = v.first;
+    last = v.last;
+    mem = new T[N];
+    for (int i = last; i != first; i = next(i))
+        mem[i] = v.mem[i];
+    if (!isEmpty())
+        mem[first] = v.mem[first];
+}
+
+template<class T>
+TQueue<T>::~TQueue() {
+    delete[] mem;
+}
+
+template<class T>
+void TQueue<T>::push(T v) {
+    if (isFull())
+        throw Exception(ExceptionType::queueisFull);
+    last = next(last);
+    mem[last] = v;
+}
+
+template<class T>
+T TQueue<T>::pop() {
+    if (isEmpty())
+        throw Exception(ExceptionType::queueisEmpty);
+    T tmp = mem[first];
+    first = next(first);
+    return tmp;
+}
+
+template<class T>
+bool TQueue<T>::isFull() {
+    return next(next(last)) == first;
+}
+
+template<class T>
+bool TQueue<T>::isEmpty() {
+    return next(last) == first;
+}
+
+template<class T>
 inline TQueue<T>::TQueue(int _N) {
     N = _N;
     mem = new T[N];
     first = 0;
     last = N - 1;
 }
+
 
 #endif  //   MODULES_SHURYGINA_REV_POL_NOTAT_INCLUDE_QUEUE_H_
 
