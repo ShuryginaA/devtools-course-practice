29a7da50210f9194140dfe4d83fa1fdd5bd1b552
ShuryginaA <annashurygina353@gmail.com>
2022-04-23 10:57:27 +0300

    refact

--- modules/shurygina_rev_pol_notat/test/test_main.cpp
+++ modules/shurygina_rev_pol_notat/test/test_main.cpp
@@ -1,63 +1,7 @@
 // Copyright 2022 Shurygina A
 
 #include <gtest/gtest.h>
-#include "include/stack.h"
-#include"include/polish_notation.h"
 
-TEST(TQueue, can_pop) {
-    TQueue<int> q(10);
-    q.push(6);
-    EXPECT_EQ(q.pop(), 6);
-}
-
-TEST(TQueue, can_create_queue_with_positive_length) {
-    TQueue<int>* s = new TQueue<int>;
-    ASSERT_NO_THROW(TQueue<int> s(5));
-    EXPECT_EQ(s->isEmpty(), true);
-}
-
-TEST(TQueue, can_create) {
-    TQueue<int>* s = new TQueue<int>;
-    ASSERT_NO_THROW(TQueue<int> s());
-}
-
-TEST(TQueue, can_create_copied_queue) {
-    TQueue<int>* s = new TQueue<int>;
-
-    ASSERT_NO_THROW(TQueue<int>*s1(s));
-}
-
-TEST(TQueue, can_push_and_pop) {
-    TQueue<int>* s = new TQueue<int>;
-    s->push(3);
-    int t = s->pop();
-    EXPECT_EQ(3, t);
-}
-
-TEST(TQueue, can_assign_queue_to_itself) {
-    TQueue<int>* s = new TQueue<int>;
-    TQueue<int>* s1(s);
-    s = s;
-    EXPECT_EQ(s, s1);
-}
-
-TEST(TQueue, Check_Queue1) {
-    TQueue<int> s(1);
-    ASSERT_NO_THROW(s.isEmpty());
-    EXPECT_EQ(s.isEmpty(), true);
-}
-
-TEST(TQueue, Check_Queue2) {
-    PolishNotation l1;
-    TQueue<Lexem*>* lex = new TQueue<Lexem*>;
-    ASSERT_NO_THROW(lex->isFull());
-    EXPECT_EQ(lex->isFull(), false);
-}
-
-TEST(TQueue, next) {
-    TQueue<int> s(1);
-    EXPECT_EQ(s.next(0), 0);
-}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
--- modules/shurygina_rev_pol_notat/test/test_polish_notation.cpp
+++ modules/shurygina_rev_pol_notat/test/test_polish_notation.cpp
@@ -5,6 +5,60 @@
 #include "include/polish_notation.h"
 #include "include/stack.h"
 
+TEST(TQueue, can_pop) {
+    TQueue<int> q(10);
+    q.push(6);
+    EXPECT_EQ(q.pop(), 6);
+}
+
+TEST(TQueue, can_create_queue_with_positive_length) {
+    TQueue<int>* s = new TQueue<int>;
+    ASSERT_NO_THROW(TQueue<int> s(5));
+    EXPECT_EQ(s->isEmpty(), true);
+}
+
+TEST(TQueue, can_create) {
+    TQueue<int>* s = new TQueue<int>;
+    ASSERT_NO_THROW(TQueue<int> s());
+}
+
+TEST(TQueue, can_create_copied_queue) {
+    TQueue<int>* s = new TQueue<int>;
+
+    ASSERT_NO_THROW(TQueue<int>*s1(s));
+}
+
+TEST(TQueue, can_push_and_pop) {
+    TQueue<int>* s = new TQueue<int>;
+    s->push(3);
+    int t = s->pop();
+    EXPECT_EQ(3, t);
+}
+
+TEST(TQueue, can_assign_queue_to_itself) {
+    TQueue<int>* s = new TQueue<int>;
+    TQueue<int>* s1(s);
+    s = s;
+    EXPECT_EQ(s, s1);
+}
+
+TEST(TQueue, Check_Queue1) {
+    TQueue<int> s(1);
+    ASSERT_NO_THROW(s.isEmpty());
+    EXPECT_EQ(s.isEmpty(), true);
+}
+
+TEST(TQueue, Check_Queue2) {
+    PolishNotation l1;
+    TQueue<Lexem*>* lex = new TQueue<Lexem*>;
+    ASSERT_NO_THROW(lex->isFull());
+    EXPECT_EQ(lex->isFull(), false);
+}
+
+TEST(TQueue, next) {
+    TQueue<int> s(1);
+    EXPECT_EQ(s.next(0), 0);
+}
 TEST(Lexem, can_create) {
     Lexem* l = new Lexem();
     Lexem l1("/", op, -1);
